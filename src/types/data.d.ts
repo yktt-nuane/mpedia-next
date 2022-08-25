b3fa25648f86c25ff6802ef3f75747d1141413ce
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-08-25 17:58:50 +0900

    recover userprofile

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,39 +1,39 @@
 // ユーザー
 export type User = {
-  id: number;
-  username: string;
-  displayName: string;
-  email: string;
-  description: string;
-};
+  id: number
+  username: string
+  displayName: string
+  email: string
+  description: string
+}
 
 // 記事
 export type Article = {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  body: string;
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  body: string
   eye_catch: {
-    url: string;
-    height: number;
-    width: number;
-  };
-  tags: Tag[];
-};
+    url: string
+    height: number
+    width: number
+  }
+  tags: Tag[]
+}
 
 export type Tag = {
-  id: string;
-  tag_name: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-};
+  id: string
+  tag_name: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
 
 // APIコンテキスト
 export type ApiContext = {
-  apiRootUrl: string;
-};
+  apiRootUrl: string
+}
